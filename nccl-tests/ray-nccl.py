a8ac21e7e65c9bf6099e4847903cece91f314c42
Peter Andreas Entschev <peter@entschev.com>
2024-11-26 10:34:17 -0800

    Setup `NCCLActor` as named actor
    Broadcast root ID from the root actor.

--- nccl-tests/ray-nccl.py
+++ nccl-tests/ray-nccl.py
@@ -11,6 +11,7 @@ class NCCLActor:
         os.environ["CUDA_VISIBLE_DEVICES"] = str(index)
         os.environ["NCCL_DEBUG"] = "DEBUG"
         self._index = index
+        self._name = f"NCCLActor-{self._index}"
         self._pool_size = pool_size
         self._is_root = True if not index else False
         self.cb = Comms(
@@ -19,9 +20,26 @@ class NCCLActor:
         self.cb.init()
         self.uniqueId = self.cb.uniqueId
         self.root_uniqueId = self.uniqueId if self._index == 0 else None
-    
-    # _func_init_nccl
+
+    def broadcast_root_unique_id(self):
+        if self._index == 0:
+            actor_handles = [ray.get_actor(name=f"NCCLActor-{i}", namespace=None) for i in range(1, pool_size)]
+            futures = [actor.set_root_unique_id.remote(self.root_uniqueId) for actor in actor_handles]
+
+            # Block until all futures complete
+            ray.get(futures)
+
+            return True
+        else:
+            raise RuntimeError("This method should only be called by the root")
+
     def setup_nccl(self):
+        if self.root_uniqueId is None:
+            raise RuntimeError(
+                "The unique ID of root is not set. Make sure `broadcast_root_unique_id` "
+                "runs on the root before calling this method."
+            )
+
         try:
             n = nccl()
             print("     Begin NCCL init", flush=True)
@@ -30,22 +48,19 @@ class NCCLActor:
             return True
         except Exception as e:
             print(f"An error occurred initializing NCCL: {e}.")
-            raise  
+            raise
             return False
-    
-    def set_root_uniqueID(self, root_uniqueId):
+
+    def set_root_unique_id(self, root_uniqueId):
+        print(f"{self._name}: set_root_unique_id")
         if self.root_uniqueId is None:
             self.root_uniqueId = root_uniqueId
-
-    def get_root_uniqueID(self):
-        return self.root_uniqueId
 
     def send_message(self, message, actor_pool):
         for i in range(self._pool_size):
             if i != self._index:
                 print("Send to Actor", i, flush=True)
                 actor_pool[i].receive_message.remote(message, self._index)
-                
 
     def receive_message(self, message, sender_index):
         print(f"Actor {self._index} received message from Actor {sender_index}: {message}", flush=True)
@@ -56,14 +71,13 @@ if not ray.is_initialized():
 
 # Start 3 Workers
 pool_size = 3
-actor_pool = [NCCLActor.remote(i, pool_size) for i in range(pool_size)]
+actor_pool = [NCCLActor.options(name=f"NCCLActor-{i}").remote(i, pool_size) for i in range(pool_size)]
 
-# Get the uniqueID from rank-0 and broadcast to all other workers
-parent_unique = ray.get(actor_pool[0].get_root_uniqueID.remote())
-[a.set_root_uniqueID.remote(parent_unique) for a in actor_pool]
+# ray.get() blocks until this completes, required before calling `setup_nccl()`
+# on non-root nodes.
+ray.get(actor_pool[0].broadcast_root_unique_id.remote())
 
 ray.get([actor_pool[i].setup_nccl.remote() for i in range(pool_size)])
-
 
 # ray.get([actor_pool[i].send_message.remote("Hello, world!", actor_pool) for i in range(pool_size)])
 
