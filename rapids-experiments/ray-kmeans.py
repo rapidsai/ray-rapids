3423d9860d439500da9ff6d9186125f8bf1a09b7
Peter Andreas Entschev <peter@entschev.com>
2024-12-13 11:55:19 -0800

    Update k-means example to use `RAFTActor` as base class

--- rapids-experiments/ray-kmeans.py
+++ rapids-experiments/ray-kmeans.py
@@ -9,72 +9,18 @@ from ray_comms import Comms
 import numpy as np
 
 from cuml_utils import make_blobs
+from raft_actor import RAFTActor
 
 @ray.remote(num_gpus=1)
-class NCCLActor:
+class KMeansActor(RAFTActor):
     def __init__(self, index, pool_size, session_id):
-        os.environ["CUDA_VISIBLE_DEVICES"] = str(index)
-        os.environ["NCCL_DEBUG"] = "DEBUG"
-        self._index = index
-        self._name = f"NCCLActor-{self._index}"
-        self._pool_size = pool_size
-        self._is_root = True if not index else False
-        self.cb = Comms(
-            verbose=True, nccl_root_location='ray-actor'
-        )
-        self.cb.init()
-        self.uniqueId = self.cb.uniqueId
-        self.root_uniqueId = self.uniqueId if self._index == 0 else None
-        self.session_id = session_id
-
-    def broadcast_root_unique_id(self):
-        if self._index == 0:
-            actor_handles = [ray.get_actor(name=f"NCCLActor-{i}", namespace=None) for i in range(1, pool_size)]
-            futures = [actor.set_root_unique_id.remote(self.root_uniqueId) for actor in actor_handles]
-
-            # Block until all futures complete
-            ray.get(futures)
-
-            return True
-        else:
-            raise RuntimeError("This method should only be called by the root")
-
-    def _setup_nccl(self):
-        self._nccl = nccl()
-        self._nccl.init(self._pool_size, self.root_uniqueId, self._index)
-
-    def _setup_raft(self):
-        self._raft_handle = Handle(n_streams=0)
-
-        inject_comms_on_handle_coll_only(self._raft_handle, self._nccl, self._pool_size, self._index, verbose=True)
+        super().__init__(index=index, pool_size=pool_size, session_id=session_id, actor_name_prefix="KMeans")
 
     def execute(self, func, *args, **kwargs):
         """
         Dangerous!  Execute arbitrary functions
         """
         return func(self, *args, **kwargs)
-
-    def setup(self):
-        if self.root_uniqueId is None:
-            raise RuntimeError(
-                "The unique ID of root is not set. Make sure `broadcast_root_unique_id` "
-                "runs on the root before calling this method."
-            )
-
-        try:
-            print("     Setting up NCCL...", flush=True)
-            self._setup_nccl()
-            print("     Setting up RAFT...", flush=True)
-            self._setup_raft()
-            print("     Setup complete!", flush=True)
-        except Exception as e:
-            print(f"An error occurred while setting up: {e}.")
-            raise
-
-    def set_root_unique_id(self, root_uniqueId):
-        print(f"{self._name}: set_root_unique_id")
-        if self.root_uniqueId is None:
-            self.root_uniqueId = root_uniqueId
 
     def set_new_variable(self, name, value):
         setattr(self, name, value)
@@ -119,10 +65,10 @@ session_id = uuid.uuid4().bytes
 
 # Start 4 Workers
 pool_size = 4
-actor_pool = [NCCLActor.options(name=f"NCCLActor-{i}").remote(i, pool_size, session_id=session_id) for i in range(pool_size)]
+actor_pool = [KMeansActor.options(name=f"KMeansActor-{i}").remote(i, pool_size, session_id=session_id) for i in range(pool_size)]
 # ray.get() blocks until this completes, required before calling `setup()`
 # on non-root nodes.
-root_actor = ray.get_actor(name="NCCLActor-0", namespace=None)
+root_actor = ray.get_actor(name="KMeansActor-0", namespace=None)
 ray.get(root_actor.broadcast_root_unique_id.remote())
 
 # Setup Comms (NCCL/Sub-communicator)
