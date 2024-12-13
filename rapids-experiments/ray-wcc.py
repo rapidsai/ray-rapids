23536f7c71f09369e92da4cf18f2d7f1780bef9e
Peter Andreas Entschev <peter@entschev.com>
2024-12-13 12:08:23 -0800

    Update WCC example to use `RAFTActor` as base class

--- rapids-experiments/ray-wcc.py
+++ rapids-experiments/ray-wcc.py
@@ -5,6 +5,7 @@ from raft_dask.common.nccl import nccl
 
 from pylibraft.common.handle import Handle
 from raft_dask.common.comms_utils import inject_comms_on_handle_coll_only
+from raft_actor import RAFTActor
 from ray_comms import Comms
 import cudf
 from ray.util.actor_pool import ActorPool
@@ -18,83 +19,18 @@ from cugraph.dask.comms.comms_wrapper import init_subcomms as c_init_subcomms
 
 
 @ray.remote(num_gpus=1)
-class NCCLActor:
+class WCCActor(RAFTActor):
     def __init__(self, index, pool_size, session_id):
-        os.environ["CUDA_VISIBLE_DEVICES"] = str(index)
-        os.environ["NCCL_DEBUG"] = "DEBUG"
-        self._index = index
-        self._name = f"NCCLActor-{self._index}"
-        self._pool_size = pool_size
-        self._is_root = True if not index else False
-        self.cb = Comms(
-            verbose=True, nccl_root_location='ray-actor'
-        )
-        self.cb.init()
-        self.uniqueId = self.cb.uniqueId
-        self.root_uniqueId = self.uniqueId if self._index == 0 else None
-        self.session_id = session_id
-
-
-
-    def broadcast_root_unique_id(self):
-        if self._index == 0:
-            actor_handles = [ray.get_actor(name=f"NCCLActor-{i}", namespace=None) for i in range(1, pool_size)]
-            futures = [actor.set_root_unique_id.remote(self.root_uniqueId) for actor in actor_handles]
-
-            # Block until all futures complete
-            ray.get(futures)
-
-            return True
-        else:
-            raise RuntimeError("This method should only be called by the root")
-
-    def _setup_nccl(self):
-        self._nccl = nccl()
-        self._nccl.init(self._pool_size, self.root_uniqueId, self._index)
-
-    def _setup_raft(self):
-        self._raft_handle = Handle(n_streams=0)
-
-        inject_comms_on_handle_coll_only(self._raft_handle, self._nccl, self._pool_size, self._index, verbose=True)
+        super().__init__(index=index, pool_size=pool_size, session_id=session_id, actor_name_prefix="WCC")
 
     def _setup_subcom(self):
         # setup sub-communicator (specific to cuGraph comms)
         # TODO: Understand partitioning of prows / pcols from subcomm_init
         c_init_subcomms(self._raft_handle, 2)
 
-
-    def setup(self):
-        if self.root_uniqueId is None:
-            raise RuntimeError(
-                "The unique ID of root is not set. Make sure `broadcast_root_unique_id` "
-                "runs on the root before calling this method."
-            )
-
-        try:
-            print("     Setting up NCCL...", flush=True)
-            self._setup_nccl()
-            print("     Setting up RAFT...", flush=True)
-            self._setup_raft()
-            print("     Setting up cuGraph-subcom...", flush=True)
-            self._setup_subcom()
-            print("     Setup complete!", flush=True)
-        except Exception as e:
-            print(f"An error occurred while setting up: {e}.")
-            raise
-
-    def set_root_unique_id(self, root_uniqueId):
-        print(f"{self._name}: set_root_unique_id")
-        if self.root_uniqueId is None:
-            self.root_uniqueId = root_uniqueId
-
-    def send_message(self, message, actor_pool):
-        for i in range(self._pool_size):
-            if i != self._index:
-                print("Send to Actor", i, flush=True)
-                actor_pool[i].receive_message.remote(message, self._index)
-
-    def receive_message(self, message, sender_index):
-        print(f"Actor {self._index} received message from Actor {sender_index}: {message}", flush=True)
+    def _setup_post(self):
+        print("     Setting up cuGraph-subcom...", flush=True)
+        self._setup_subcom()
 
     def weakly_connected_components(self, start, stop):
         """
@@ -148,18 +84,17 @@ class NCCLActor:
 
 # Initialize Ray
 if not ray.is_initialized():
-    # ray.init(include_dashboard=False, address="10.33.227.163:6379")
     ray.init(include_dashboard=False)
 
 session_id = uuid.uuid4().bytes
 
 # Start 4 Workers
-pool_size = 10
-actor_pool = [NCCLActor.options(name=f"NCCLActor-{i}").remote(i, pool_size, session_id=session_id) for i in range(pool_size)]
+pool_size = 4
+actor_pool = [WCCActor.options(name=f"WCCActor-{i}").remote(i, pool_size, session_id=session_id) for i in range(pool_size)]
 
 # ray.get() blocks until this completes, required before calling `setup()`
 # on non-root nodes.
-root_actor = ray.get_actor(name="NCCLActor-0", namespace=None)
+root_actor = ray.get_actor(name="WCCActor-0", namespace=None)
 ray.get(root_actor.broadcast_root_unique_id.remote())
 
 # Setup Comms (NCCL/Sub-communicator)
